5f53a05f8a633892ab614e5c1266a52e6305dec3
Fantasy Shao <forever.fantasy27@gmail.com>
2014-10-29 18:43:05 +0800

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,25 @@
  * @description
  * @author Fantasy <fantasyshao@icloud.com>
  * @create 2014-10-28
- * @update 2014-10-28
+ * @update 2014-10-29
  */
+
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+var paths = {
+  sass: 'sass/site.sass'
+};
+
+gulp.task('sass', function () {
+  gulp.src(paths.sass)
+    .pipe(sass({ style: 'compressed', noCache: true }))
+    .on('error', function (err) { console.log(err); })
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('default', ['sass']);
+
+gulp.task('watch', function () {
+  gulp.watch(paths.sass, ['sass']);
+});
