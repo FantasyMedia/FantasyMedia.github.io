6d5a88872af22ad05728d005280930b16f52f08e
Fantasy Shao <forever.fantasy27@gmail.com>
2014-10-29 21:12:01 +0800

    v0.0.1 homepage of FantasyMedia

--- scripts/site.js
+++ scripts/site.js
@@ -7,3 +7,35 @@
  * @create 2014-10-29
  * @update 2014-10-29
  */
+
+(function () {
+
+  var $ = {
+    getOne: function (str) {
+      return document.querySelector(str);
+    }
+  };
+
+  ({
+
+    init: function () {
+      this.bindEvent();
+    },
+
+    bindEvent: function () {
+      var links = $.getOne('#links');
+
+      $.getOne('#menu').onclick = function () {
+        links.classList.add('show');
+        return false;
+      };
+
+      $.getOne('#close').onclick = function () {
+        links.classList.remove('show');
+        return false;
+      };
+    }
+
+  }).init();
+
+})();
